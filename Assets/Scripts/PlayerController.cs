f553c1e4110e654e6a0f991c1ac34b7d1f03383e
Popey0424 <roylouis0424@gmail.com>
2024-09-26 11:21:24 +0200

    TP

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+    }
+
+    void HandleInputs()
+    {
+        _inputs.x = Input.GetAxisRaw("Horizontal");
+        _inputs.y = Input.GetAxisRaw("Vertical");
+
+        _inputJump = Input.GetKey(KeyCode.UpArrow);
+    }
+}
