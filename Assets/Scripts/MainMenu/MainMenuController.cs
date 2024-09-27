6d71365487b76cdb4c24448ce09e7c5a06de3e53
P3D\Louis.roy <roylouis0424@gmail.com>
2024-09-27 17:33:25 +0200

    Spike Scprit

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -15,17 +15,70 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject creditsMenu;
     [SerializeField] private GameObject settignsMenu;
+    [SerializeField] private GameObject leaveWarning;
 
-   
+    [SerializeField] private GameObject rayCastLeaveImage;
+
+    public KeyCode leaveMainMenu;
 
 
 
 
+
+    #region StartGame
     public void OnClickPlaybutton()
     {
-
+        imagefade.gameObject.SetActive(true);
+        imagefade.DOFade(1, 2.9f).OnComplete(FadeStartComplete);
     }
-    
+    private void FadeStartComplete()
+    {
+        SceneManager.LoadScene(2);
+    }
+    #endregion
+
+    #region CreditsMenu
+    public void OnClickCreditsButton()
+    {
+        imagefade.gameObject.SetActive(true);
+        imagefade.DOFade(1, 2.9f).OnComplete(FadeCreditsComplete);
+    }
+    private void FadeCreditsComplete()
+    {
+        creditsMenu.gameObject.SetActive(true);
+    }
+    #endregion
+
+    #region SettingsMenu
+    public void OnClickSettingsButtons()
+    {
+        imagefade.gameObject.SetActive(true);
+        imagefade.DOFade(1, 1f).OnComplete(FadeSettingsComplete);
+    }
+
+    private void FadeSettingsComplete()
+    {
+        settignsMenu.gameObject.SetActive(true);
+    }
+    #endregion
+
+    #region LaveGame
+    public void OnClickLeaveGameButton()
+    {
+        Animator animator_LeaveGame = leaveWarning.GetComponent<Animator>();
+
+        if (animator_LeaveGame != null)
+        {
+            bool IsOpen = animator_LeaveGame.GetBool("IsOpen");
+            animator_LeaveGame.SetBool("IsOpen", true);
+        }
+        rayCastLeaveImage.SetActive(true);
+    }
+    #endregion
+
+
+
+
 
 
 
