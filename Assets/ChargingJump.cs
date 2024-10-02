29cf49206ef4d63a1852aa6498878c3acddd87ab
MurMiauShaurmiau <144931088+MurMiauShaurmiau@users.noreply.github.com>
2024-10-02 11:18:18 +0200

    code_asset_problemes_slider

--- Assets/ChargingJump.cs
+++ Assets/ChargingJump.cs
@@ -3,13 +3,13 @@ using UnityEngine.UI;
 
 public class JumpChargeUI : MonoBehaviour
 {
-    [SerializeField] private Slider _jumpChargeSlider; 
+    [SerializeField] private Image _jumpChargeSlider; 
     [SerializeField] private PlayerController _playerController; 
 
     private void Start()
     {
         
-        _jumpChargeSlider.value = 0;
+        _jumpChargeSlider.fillAmount = 0;
     }
 
     private void Update()
@@ -18,12 +18,12 @@ public class JumpChargeUI : MonoBehaviour
         if (_playerController.IsChargingJump())
         {
             
-            _jumpChargeSlider.value = _playerController.GetJumpChargePercentage();
+            _jumpChargeSlider.fillAmount = _playerController.GetJumpChargePercentage();
         }
         else
         {
             
-            _jumpChargeSlider.value = 0;
+            _jumpChargeSlider.fillAmount = 0;
         }
     }
 }
