c3bf1e1c22ec9f1a287f745c2d79c7e36609bba1
Sorako-Kun <114805504+Sorako-Kun@users.noreply.github.com>
2024-10-04 11:57:14 +0200

    Intro

---
+++ Assets/Scripts/Direction_Scene/Direction_Game.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Direction_Game : MonoBehaviour
+{
+    // Durée du délai avant de changer de scène (en secondes)
+    public float delay = 5f;
+
+    // Démarre la coroutine lors du chargement de la scène
+    void Start()
+    {
+        StartCoroutine(GoToMainMenu());
+    }
+
+    // Coroutine pour attendre un délai avant de changer de scène
+    IEnumerator GoToMainMenu()
+    {
+        // Attend pendant "delay" secondes
+        yield return new WaitForSeconds(delay);
+
+        // Change de scène pour aller au menu principal
+        SceneManager.LoadScene("SceneGD");
+    }
+}
